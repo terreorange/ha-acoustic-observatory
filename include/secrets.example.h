32fa064cd83b074718522476aa4cdaf91d1bb213
terreorange <127016566+terreorange@users.noreply.github.com>
2026-07-26 10:06:22 +0200

    Remove root secrets example after moving firmware

--- include/secrets.example.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define WIFI_SSID "VOTRE_WIFI"
-#define WIFI_PASSWORD "VOTRE_MOT_DE_PASSE_WIFI"
-
-#define MQTT_HOST "homeassistant.local"
-#define MQTT_PORT 1883
-#define MQTT_USER "atom_echo"
-#define MQTT_PASSWORD "VOTRE_MOT_DE_PASSE_MQTT"
